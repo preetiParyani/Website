1947a5b1f6c8d62ee542968dea399f4cd16e24ce
Preeti Paryani <preetiparyani29@gmail.com>
2024-04-30 23:41:30 +0530

    Created until the li's with the image thing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('main'),
-    smooth: true
+console.log("hua");
+
+let elemC = document.querySelector("#elem-container");
+let fixed = document.querySelector("#fixed-container");
+elemC.addEventListener("mouseenter", function () {
+  fixed.style.display = "block";
 });
+elemC.addEventListener("mouseleave", function () {
+    fixed.style.display = "none"
+})
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e) {
+    e.addEventListener("mouseenter", function() {
+        var image = e.getAttribute( "data-image" );
+        fixed.style.backgroundImage  = `url('${image}')` ;
+    })
+})
+
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("main"),
+  smooth: true,
+});
+
